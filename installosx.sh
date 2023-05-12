ffb44e99202f7ede27e1afa4547108a54a7b5c58
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2023-05-12 17:28:23 +0300

    🔧 chore(installosx.sh): remove unnecessary commands and comments, add command to show hidden files by default The script now only includes necessary commands to improve the user's experience. The command to show hidden files by default has been added to make it easier for the user to navigate through the file system. The unnecessary commands and comments have been removed to make the script more concise and easier to read.

--- installosx.sh
+++ installosx.sh
@@ -1,4 +1,5 @@
-# Pre-requisite - system ssh agent setup  with keys
+echo 'Show hidden files by default'
+defaults write com.apple.Finder AppleShowAllFiles
 
 echo 'Setting up a blazingly fast keyboard repeat rate'
 defaults write NSGlobalDomain KeyRepeat -int 1
@@ -6,26 +7,16 @@ defaults write NSGlobalDomain KeyRepeat -int 1
 echo 'Setting up a shorter delay until key repeat'
 defaults write NSGlobalDomain InitialKeyRepeat -int 10
 
-echo 'Disabling key Character Accent Menu (foreing characters like ø)'
-defaults write -g ApplePressAndHoldEnabled -bool false
-
-echo 'Installing Powerline Fonts (spaceship-prompt dependency)'
-git clone git@github.com:powerline/fonts.git ~/fonts && sh ~/fonts/install.sh && rm -rf ~/fonts
-
-echo 'Installing Nerd Fonts'
-git clone git@github.com:ryanoasis/nerd-fonts.git ~/nerd-fonts && cd ~/nerd-fonts && ./install.sh && cd ~ && rm -rf ~/nerd-fonts
-
 echo 'Installing classic formulas'
 brew install exa bat the_silver_searcher fzf httpie rename
+# exa — a modern version of ls.
+# bat — A cat(1) clone with wings.
 # the_silver_searcher — a code searching tool similar to ack, with a focus on speed (source code).
 # fzf — 🌸 A command-line fuzzy finder (directories).
 # httpie — is a command line HTTP client with an intuitive UI, JSON support, syntax highlighting, wget-like downloads, plugins, and more.
-# exa — a modern version of ls.
 # rename — rename [switches|transforms] [files] (http://plasmasturm.org/code/rename/).
-# bat — A cat(1) clone with wings.
 # ‼️  do not forget to install z.
 
 # Install Vim-Plug into ~/.vim/autoload
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
