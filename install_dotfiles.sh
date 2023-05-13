784cab24aa8eeb0f6c46e123fa0b112fd9708392
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2023-05-13 16:44:06 +0300

    🔥 chore(install_dotfiles.sh): remove unused files variable 👌 refactor(install_dotfiles.sh): update dotfile_list variable to include all dotfiles to symlink in homedir

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -7,7 +7,7 @@
 dotfiles_source_dir=~/.dotfiles/.source # ? dotfiles source directory
 dotfiles_backup_dir=~/.dotfiles_backup  # ? dotfiles backup directory
 dotfile_list=".hushlogin"               # ? list of files/folders to symlink in homedir
-# files=".zshrc .zprofile .zshenv .zshrc .vimrc .yarnrc .hushlogin" # ? list of files/folders to symlink in homedir. Description of zsh files https://apple.stackexchange.com/questions/388622/zsh-zprofile-zshrc-zlogin-what-goes-where
+# files=".zshrc .zprofile .zshenv .vimrc .yarnrc .hushlogin" # ? list of files/folders to symlink in homedir. Description of zsh files https://apple.stackexchange.com/questions/388622/zsh-zprofile-zshrc-zlogin-what-goes-where
 
 # TODO
 # ~/.config/oh-my-zsh-custom/functions.zsh
