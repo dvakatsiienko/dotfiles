6927e52d1e53ab3074c8df0ca492382c183ab7da
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2023-05-14 18:51:21 +0300

    🎨 refactor(install_macos): rewrite install_macos.sh as install_macos.mjs using ES modules and zx library to improve readability and maintainability

---
+++ script/install_macos.mjs
@@ -0,0 +1,35 @@
+/**
+ * ? This script:
+ * ? - ...
+ */
+
+/* Core */
+import * as zx from 'zx';
+
+/* Instruments */
+import { bb, yb, mb, gb, rb, new_line } from './lib.mjs';
+
+zx.echo('test');
+
+// echo 'Show hidden files by default'
+// defaults write com.apple.Finder AppleShowAllFiles
+
+// echo 'Setting up a blazingly fast keyboard repeat rate'
+// defaults write NSGlobalDomain KeyRepeat -int 1
+
+// echo 'Setting up a shorter delay until key repeat'
+// defaults write NSGlobalDomain InitialKeyRepeat -int 10
+
+// echo 'Installing classic formulas'
+// brew install exa bat the_silver_searcher fzf httpie rename
+// # exa — a modern version of ls.
+// # bat — A cat(1) clone with wings.
+// # the_silver_searcher — a code searching tool similar to ack, with a focus on speed (source code).
+// # fzf — 🌸 A command-line fuzzy finder (directories).
+// # httpie — is a command line HTTP client with an intuitive UI, JSON support, syntax highlighting, wget-like downloads, plugins, and more.
+// # rename — rename [switches|transforms] [files] (http://plasmasturm.org/code/rename/).
+// # ‼️  do not forget to install z.
+
+// # Install Vim-Plug into ~/.vim/autoload
+// curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+//     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
