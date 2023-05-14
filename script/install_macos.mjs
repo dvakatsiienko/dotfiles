3eb3a77982c56866dce910aa63dc7b73a21d3f43
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2023-05-14 19:17:29 +0300

    ✨ feat(install_macos.mjs): add macOS setup script to configure system defaults, install brew, classic brew formulaes, and vim-plug.

--- script/install_macos.mjs
+++ script/install_macos.mjs
@@ -9,27 +9,67 @@ import * as zx from 'zx';
 /* Instruments */
 import { bb, yb, mb, gb, rb, new_line } from './lib.mjs';
 
-zx.echo('test');
+zx.echo(gb('🏁 Initiating macOS setup.'));
+new_line();
 
-// echo 'Show hidden files by default'
-// defaults write com.apple.Finder AppleShowAllFiles
+zx.echo(bb('🖥️  Configuring macOS defaults...'));
+new_line();
 
-// echo 'Setting up a blazingly fast keyboard repeat rate'
-// defaults write NSGlobalDomain KeyRepeat -int 1
+zx.echo(bb(`Show ${ mb('hidden files') } by default.`));
+await zx.$`defaults write com.apple.finder AppleShowAllFiles true`;
+new_line();
 
-// echo 'Setting up a shorter delay until key repeat'
-// defaults write NSGlobalDomain InitialKeyRepeat -int 10
+zx.echo(bb(`Setting up a ${ mb('blazingly fast keypress repeat rate') }.`));
+await zx.$`defaults write NSGlobalDomain KeyRepeat -int 1`;
+new_line();
 
-// echo 'Installing classic formulas'
-// brew install exa bat the_silver_searcher fzf httpie rename
-// # exa — a modern version of ls.
-// # bat — A cat(1) clone with wings.
-// # the_silver_searcher — a code searching tool similar to ack, with a focus on speed (source code).
-// # fzf — 🌸 A command-line fuzzy finder (directories).
-// # httpie — is a command line HTTP client with an intuitive UI, JSON support, syntax highlighting, wget-like downloads, plugins, and more.
-// # rename — rename [switches|transforms] [files] (http://plasmasturm.org/code/rename/).
-// # ‼️  do not forget to install z.
+zx.echo(bb(`Setting up a ${ mb('shorter delay until key repeat') }.`));
+await zx.$` defaults write NSGlobalDomain InitialKeyRepeat -int 10`;
+new_line();
 
-// # Install Vim-Plug into ~/.vim/autoload
-// curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-//     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+zx.echo(bb(`🍻 Initializing ${ mb('brew') }...`));
+new_line();
+
+const is_brew_installed = await check_is_brew_installed();
+
+if (!is_brew_installed) {
+    zx.echo(rb(`Error: ${ mb('brew') } is not installed. Installing...`));
+    try {
+        await zx.$`/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"`;
+        zx.echo(gb(`✓ ${ mb('brew') } is now installed. Proceeding...`));
+    } catch {
+        zx.echo(rb(`Failed to install ${ mb('brew') }. Aborting.`));
+        await zx.$`exit 1`;
+    }
+} else {
+    zx.echo(gb(`✓ ${ mb('brew') } is installed. Proceeding...`));
+}
+
+new_line();
+
+zx.echo(bb(`🍺 Installing classic ${ mb('brew') } formulaes.`));
+await zx.$`brew install exa bat the_silver_searcher fzf vim`;
+// ? exa — a modern version of ls.
+// ? bat — A cat(1) clone with wings.
+// ? the_silver_searcher — a code searching tool similar to ack, with a focus on speed (source code).
+// ? fzf — 🌸 A command-line fuzzy finder (directories).
+// ? vim
+
+zx.echo(`🔌 Installing ${ mb('vim-plug') } into ${ yb('~/.vim/autoload') }`);
+await zx.$`curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim`;
+
+new_line();
+zx.echo('✅ Done.');
+
+/* Helpers */
+async function check_is_brew_installed () {
+    zx.echo(bb(`🔎 Checking if ${ mb('brew') } binary is installed in the sytem.`));
+
+    try {
+        await zx.$`which brew`;
+    } catch {
+        return false;
+    }
+
+    return true;
+}
